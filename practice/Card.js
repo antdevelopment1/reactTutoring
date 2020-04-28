af713fc91a24a51554ba1fe992c43cca6798d3bf
April Copes <antdevelopment1@gmail.com>
2020-04-27 22:55:35 -0400

    continues to refactor and introduce various options

--- practice/Card.js
+++ practice/Card.js
@@ -5,11 +5,11 @@ function Card(props){
         <div className="col s2">
             <div className="card hoverable small">
                 <div className="card-image">
-                    <img src={props.iamge} />
+                    <img src={props.data.image} />
                 </div>
                 <div className="card-content">
-                    <p>{props.title}</p>
-                    <p>{props.name}</p>
+                    <p>{props.data.course}</p>
+                    <p>{props.data.instructor}</p>
                 </div>
                 <div className="card-action">
                     <a href="#">$9.99</a>
