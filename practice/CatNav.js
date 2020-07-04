9a3e3c021a7d57f3b71d27266e9d643b46da0f7d
April Copes <antdevelopment1@gmail.com>
2020-07-04 00:52:11 -0400

    Adds modified files

--- practice/CatNav.js
+++ practice/CatNav.js
@@ -11,31 +11,17 @@
 
 class CatNav extends React.Component {
     render() {
+        
         const navLinks = this.props.data.map( (link, i) => {
             return (
                 <li key={i} className="cat-link left valign-wrapper">
                     <i className="material-icons">{link.icon}</i>{link.title}
                 </li>
-                )
+            )
         })
-           
-        console.log(navLinks)
-    
-        return(
-            <div className="row">
-                <ul className="cat-nav center-align">
-                    {navLinks}
-                </ul>
-            </div>
+
+        return (
+            <div>{navLinks}</div>
         )
     }
-    // constructor() {
-    //     super();
-    // }
-
-    // render() {
-    //     return (
-    //         <div>{this.props}</div>
-    //     )
-    // }
 }
